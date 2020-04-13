52f909011ba37390390b81e21587ac65aab8dd59
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-13 11:58:26 +0300

    project created.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AopExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
